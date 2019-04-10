e0f71eb84ed6fb267d8f31bd3e113242eedf2cec
110619008 <a50208pass@gmail.com>
2019-04-11 02:45:12 +0800

    110619008-final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,29 @@
 let $ = require('jquery');
+import bird from '../images/bird.jpg'
+import squirrel from '../images/squirrel.jpg'
 
-import bird from '../../images/bird.jpg';
-import squirrel from '../../images/squirrel.jpg';
-import '../../css/normalize.css';
-import '../../css/style.css';
+import '../css/style.css';
+import '../css/normalize.css';
 
-$('#image__bird').attr("src",bird);
-$('#image__squirrel').attr("src",squirrel);
+import "../fonts/PT-Sans.eot";
+import "../fonts/PT-Sans-Bold.eot";
+import "../fonts/PT-Sans-Italic.eot";
+import "../fonts/PT-Sans-BoldItalic.eot";
+import "../fonts/PT-Sans.woff";
+import "../fonts/PT-Sans-Bold.woff";
+import "../fonts/PT-Sans-Italic.woff";
+import "../fonts/PT-Sans-BoldItalic.woff";
+import "../fonts/PT-Sans.ttf";
+import "../fonts/PT-Sans-Bold.ttf";
+import "../fonts/PT-Sans-Italic.ttf";
+import "../fonts/PT-Sans-BoldItalic.ttf";
+import "../fonts/PT-Sans.svg";
+import "../fonts/PT-Sans-Bold.svg";
+import "../fonts/PT-Sans-Italic.svg";
+import "../fonts/PT-Sans-BoldItalic.svg";
+
+$('#image_bird').attr("src", bird);
+$('#image_squirrel').attr("src", squirrel);
 
 $(document).ready(function () {
 	// when a hanburger menu is clicked
